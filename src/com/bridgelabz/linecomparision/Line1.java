c9ce2a9fb0a47a05bdae617debdce86fc94d12e1
shiwani <shiwanipaunikar1@gmail.com>
2022-11-22 13:46:50 -0800

    [shiwani]length of line is calulated program added

---
+++ src/com/bridgelabz/linecomparision/Line1.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+public class Line1 {
+    public static void main(String[] args) {
+        int x1,x2,y1,y2;
+        double Length_Of_Line;
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter x1 point");
+        x1=scan.nextInt();
+        System.out.println("enter x2 point");
+        x2=scan.nextInt();
+        System.out.println("enter y1 point");
+        y1=scan.nextInt();
+        System.out.println("enter y2 point");
+        y2=scan.nextInt();
+        Length_Of_Line=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+        System.out.println("distance between point is"+"("+x1+","+x2+"),"+"("+y1+","+y2+")="+Length_Of_Line);
+
+    }
+
+
+}
