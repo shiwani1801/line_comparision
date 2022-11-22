8c8b34a06d86b3da2a73638a104ae71c3162cf9a
shiwani <shiwanipaunikar1@gmail.com>
2022-11-22 13:42:00 -0800

    [shiwani]line comparision program

---
+++ src/com/bridgelabz/linecomparision/LineComparision.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparision;
+
+import java.util.Scanner;
+
+public class LineComparision {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Line Comparision Program ");
+    }
+}
